3fc9536063d78a1ac7f2e297ffa0601ffccb9c02
Damjan Pavlica <mudroljub@gmail.com>
2018-12-20 23:04:33 +0100

    rename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Zdravo SVete</h1>
+        <h1>Novo Groblje App</h1>
         <Filteri/>
         <Mapa/>
       </div>
