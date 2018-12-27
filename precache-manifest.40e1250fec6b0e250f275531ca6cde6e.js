4d60f3fd2f3df8010d9390f2c9571331c518b27d
Damjan Pavlica <mudroljub@gmail.com>
2018-12-27 19:20:04 +0100

    Updates

---
+++ precache-manifest.40e1250fec6b0e250f275531ca6cde6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c08f5a0b0a5c005d5a0160b0058ea1",
+    "url": "/static/media/location-pin.c8c08f5a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04530f5d1a37f408c6bc",
+    "url": "/static/js/main.04530f5d.chunk.js"
+  },
+  {
+    "revision": "38caa3db393ada55ad61",
+    "url": "/static/js/1.38caa3db.chunk.js"
+  },
+  {
+    "revision": "04530f5d1a37f408c6bc",
+    "url": "/static/css/main.1eff8dca.chunk.css"
+  },
+  {
+    "revision": "8469716be4404ae45f684582673e1e90",
+    "url": "/index.html"
+  }
+];
