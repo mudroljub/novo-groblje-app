29204201c18b5719b25756f38889f9e612da39ea
Damjan Pavlica <mudroljub@gmail.com>
2019-01-03 20:51:54 +0100

    Updates

---
+++ precache-manifest.ad2af24c83727a7aaf129efb1c1c4af0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "de6624f009392024ce0c0fc3499af9ab",
+    "url": "/novo-groblje-app/static/media/vinarija.de6624f0.svg"
+  },
+  {
+    "revision": "f9371e61fd53a34e47544365f5a096eb",
+    "url": "/novo-groblje-app/static/media/spomenik.f9371e61.svg"
+  },
+  {
+    "revision": "99d4ce263792f3baccf21734aad8348c",
+    "url": "/novo-groblje-app/static/media/restoran.99d4ce26.svg"
+  },
+  {
+    "revision": "c8c08f5a0b0a5c005d5a0160b0058ea1",
+    "url": "/novo-groblje-app/static/media/location-pin.c8c08f5a.svg"
+  },
+  {
+    "revision": "10836955631cb00604be",
+    "url": "/novo-groblje-app/static/js/runtime~main.10836955.js"
+  },
+  {
+    "revision": "1905791dac63a642e808",
+    "url": "/novo-groblje-app/static/js/main.1905791d.chunk.js"
+  },
+  {
+    "revision": "5acfb65fb7e8fd3f0e9c",
+    "url": "/novo-groblje-app/static/js/1.5acfb65f.chunk.js"
+  },
+  {
+    "revision": "1905791dac63a642e808",
+    "url": "/novo-groblje-app/static/css/main.c179aaad.chunk.css"
+  },
+  {
+    "revision": "51002b94f0a80de48188c7c26c69360d",
+    "url": "/novo-groblje-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/novo-groblje-app/precache-manifest.94ab3c1c36b4a7c2f063c9a697e712a0.js"
+  "/novo-groblje-app/precache-manifest.ad2af24c83727a7aaf129efb1c1c4af0.js"
 );
 
 workbox.clientsClaim();
